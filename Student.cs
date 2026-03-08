02ac6598edecaf71ba91aedd9ff40791751cd363
raifa-alhaj <ymtw8m9ycw@privaterelay.appleid.com>
2026-03-08 03:03:10 -0400

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StudentCodeFirstApp
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
